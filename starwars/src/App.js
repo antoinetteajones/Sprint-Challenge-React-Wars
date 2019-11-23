b5c57e06cce4f532298546aea38c79b4839b79f7
Antoinette <create@aplusmedia.info>
2019-11-22 22:35:45 -0500

    finally got something to show on the screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,14 +4,13 @@ import axios from "axios";
 import StarWars from "../src/components/StarWars";
 
 const App = () => {
-  const [war, setWar] = useState({})
+  const [war, setWar] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 useEffect(() => {
   axios
     .get("https://swapi.co/api/people/")
       .then(response =>{
-        console.log(response.data.results)
         const war = response.data.results;
         console.log(war)
         setWar(war);
@@ -28,7 +27,7 @@ useEffect(() => {
     <div>
     {war.map(char => {
       return(
-        <StarWars 
+        <StarWars key = {char.id}
         name = {char.name}
         birth = {char.birth_year}
         gender = {char.gender}
--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -5,7 +5,10 @@ const StarWars = props => {
 
     return(
         <div>
-
+        <h1>My name is: {props.name}</h1>
+        <p>I was born in the year of {props.birth}</p>
+        <p>I am a {props.gender}</p>
+        <p>My homeworld is {props.homeworld}</p>
         </div>
     )
 }
