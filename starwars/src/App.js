075c4fbd4e35c75ee2a06274b1c3b607ae199ca4
Antoinette <create@aplusmedia.info>
2019-11-22 23:26:55 -0500

    completed project

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,41 +2,47 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
 import StarWars from "../src/components/StarWars";
+import {Container, Row} from "reactstrap";
 
 const App = () => {
   const [war, setWar] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-useEffect(() => {
-  axios
-    .get("https://swapi.co/api/people/")
-      .then(response =>{
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
         const war = response.data.results;
         console.log(war)
         setWar(war);
       })
-      .catch(error =>{
+      .catch(error => {
         console.log("ERROR:", error)
       });
-}, []);
+  }, []);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div>
-    {war.map(char => {
-      return(
-        <StarWars key = {char.id}
-        name = {char.name}
-        birth = {char.birth_year}
-        gender = {char.gender}
-        homeworld = {char.homeworld}
-        />
-      )
+    <Container>
+        <h1 className="Header">React Wars</h1>
+
+      <Row>
         
-    })}
-   </div>
+        {war.map(char => {
+          return (
+            <StarWars key={char.id}
+              name={char.name}
+              birth={char.birth_year}
+              gender={char.gender}
+              hair={char.hair_color}
+            />
+          )
+
+        })}
+      </Row>
+    </Container>
   );
 }
 
--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,15 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import {Col} from "reactstrap";
 
-
+const DarkVader = styled.div`
+ .characters{
+    background-color: lightgrey;
+    color: black;
+    width: 350px;
+    max-height: 800px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px;
+    border: 2px solid black;
+    border-radius: 20px;
+    padding: 20px;
+ }
+`
 const StarWars = props => {
 
-    return(
-        <div>
-        <h1>My name is: {props.name}</h1>
-        <p>I was born in the year of {props.birth}</p>
-        <p>I am a {props.gender}</p>
-        <p>My homeworld is {props.homeworld}</p>
-        </div>
+    return (
+        
+        <Col xs="6" sm="4" >
+
+            <DarkVader>
+                <div className="characters">
+                    <h2>My name is: {props.name}</h2>
+                    <p>I was born in the year of {props.birth}</p>
+                    <p>My gender is {props.gender}</p>
+                    <p>My hair color is {props.hair}</p>
+                </div>
+            </DarkVader>
+        </Col>
     )
 }
 export default StarWars;
